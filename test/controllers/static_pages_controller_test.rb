17b72def95213b9a681afec7cbadb94356574312
Pierre Lasante <pierre.lasante@videotron.ca>
2015-06-21 13:55:04 -0700

    Rearranged home.html.erb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Microblobs App"
+    assert_select "title", "Microblobs App"
   end
 
   test "should get help" do
