864faf52aef412b83c53cd1267496da62a1ae52c
Pierre Lasante <pierre.lasante@videotron.ca>
2015-07-01 06:44:12 -0700

    Added user modification capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,23 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
+    # This is to prevent mass assignment vulnerability
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
