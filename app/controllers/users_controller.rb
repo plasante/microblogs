4e8e2ae93c76dcb861088fba16db16ebb96924b6
Pierre Lasante <pierre.lasante@videotron.ca>
2015-07-02 04:34:18 -0700

    Reached 9.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -38,5 +40,14 @@ class UsersController < ApplicationController
     # This is to prevent mass assignment vulnerability
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # Before filters
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
