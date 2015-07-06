b234f50dee2d177ae62cac2f23244a9b636b5e7d
Pierre Lasante <pierre.lasante@videotron.ca>
2015-07-06 13:43:17 -0700

    Revert "Merge branch 'master' of https://github.com/plasante/microblogs"
    This reverts commit 4de33427e97b532e2d7a682884848b957906477e, reversing
    changes made to cd0cf232aa289d8948c99556d85b1d7f16cda7af.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      log_in @user
+      flash[:success] = "Welcome the the Microblogs App!"
+      redirect_to @user
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
   
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token
   
   has_secure_password
   
   before_save { self.email = email.downcase }
-  before_create :create_activation_digest
   
   validates :name, presence: true, length: { maximum: 50 }
   
@@ -35,22 +34,13 @@ class User < ActiveRecord::Base
   end
   
   # Returns true if the given token matches the digest.
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
   end
-  
-  private
-  
-    # Creates and assigns the activation token and digest
-    def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
 end
